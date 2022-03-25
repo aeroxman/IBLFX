41d6364aecabe504fa655990caa1a69ce8f53206
aeroxman <brasil_denis@hotmail.com>
2022-03-25 02:17:53 -0300

    prototipo landing page completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
+import React from "react";
 import "./App.css";
 import LandingPage from "./components/organisms/LandingPage";
+import LandingPage2 from "./components/organisms/LandingPage2";
 
 function App() {
-  return (
-    <LandingPage />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <h1 className="text-3xl font-bold underline text-blue-300">
-    //       Hello world!
-    //     </h1>
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+  return <LandingPage />;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        realPurple: "#1f133e",
+        realOrange: "#ff4b28",
+        realBlue: "#0c0f2b",
+      },
+      boxShadow: {
+        "3xl": "0px 0px 30px -1px rgba(0, 0, 0, 0.3)",
+      },
+    },
   },
   plugins: [],
-}
+};
